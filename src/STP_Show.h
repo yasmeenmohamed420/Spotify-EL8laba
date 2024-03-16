332d40eb6594e6356d541c7fa82d24f910e66e26
Yasmeen Mohamed <67623624+yasmeenmohamed420@users.noreply.github.com>
2024-03-16 04:47:52 +0200

    Add files via upload

---
+++ src/STP_Show.h
@@ -0,0 +1,17 @@
+/*
+ * STP_Show.h
+ *
+ *      Author: Yasmeen
+ */
+
+#ifndef STP_SHOW_H_
+#define STP_SHOW_H_
+
+void Hamoksha(void);
+void NoteMusic(void);
+void Show_Cairokee(void);
+void SHOW_AMR(void);
+void Show_Tamer(void);
+void NUM_TWO(void);
+
+#endif /* STP_SHOW_H_ */
